66c40ff660f9d77c1391260259502996ac98b8b9
PriteeAr0te <arotepritee111@gmail.com>
2024-02-10 19:45:33 +0530

    updating code

--- src/App.js
+++ src/App.js
@@ -1,59 +1,65 @@
-// import logo from './logo.svg';
-import './App.css';
-import Navbar from './Components/Navbar';
-import Alert from './Components/Alert';
-import About from './Components/About';
-import Myform from './Components/MyForm';
-import React, {useState} from "react";
-// import {Switch} from 'react-router-dom';
-import{
-  BrowserRouter as Router,
-  Routes,
-  Route
-}from "react-router-dom";
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import Alert from "./Components/Alert";
+import About from "./Components/About";
+import Myform from "./Components/MyForm";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  const[mode, setMode] = useState('light');
-  const[alert, setAlert] = useState(null);
-  
-const showAlert = (message,type) => {                                                                                                                         
-  setAlert({
-    msg: message,
-    type: type
-  })
-  setTimeout(()=>{
-    setAlert(null);
-  },2000)
-}
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
 
-  const toggleMode = () =>{
-    if(mode === 'light'){
-      setMode('dark');
-      document.body.style.backgroundColor = '#4b4c50';
-      showAlert('Dark mode has been enabled','success')
-    }
-    else{
-      setMode('light');
-      document.body.style.backgroundColor = 'white';
-      showAlert('Light mode has been enabled', 'success')
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  };
 
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#4b4c50";
+      showAlert("Dark mode has been enabled", "success");
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode has been enabled", "success");
     }
-  }
+  };
   return (
     <>
-    <Router>
-      <Navbar title ="TextUtils" aboutText ="About" mode={mode} toggleMode ={toggleMode}/>
-      <Alert alert = {alert}/>
-      <div className="container my-2">
-      <Routes>
-          <Route path="/"  element={
-                <Myform showAlert = {showAlert} heading = " Try TextUtils- Word Counter, Character Counter, Replace Word" mode={mode}/>
-                } /> 
-          <Route path="/about"  element={<About showAlert ={showAlert} mode = {mode} />} />
-            
-        </Routes>
-      </div>
-    </Router>
+      <Router>
+        <Navbar
+          title="TextUtils"
+          aboutText="About"
+          mode={mode}
+          toggleMode={toggleMode}
+        />
+        <Alert alert={alert} />
+        <div className="container my-2">
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <Myform
+                  showAlert={showAlert}
+                  heading=" Try TextUtils- Word Counter, Character Counter, Replace Word"
+                  mode={mode}
+                />
+              }
+            />
+            <Route
+              path="/about"
+              element={<About showAlert={showAlert} mode={mode} />}
+            />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
